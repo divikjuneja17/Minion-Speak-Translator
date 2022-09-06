e7a841df1e6d2889b77217ea26f8c625c010441b
Divik Juneja <junejadivik17@gmail.com>
2022-09-06 23:00:39 +0530

    included output div for translated text

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 let btnTranslate = document.querySelector("#btn-translate");
-let txtInput = document.querySelector("#textarea-input");
-let txtOutput = document.querySelector("#textarea-output");
+let txtInput = document.querySelector("#input");
+let txtOutput = document.querySelector("#output");
 
 
-// live exercise: querySelector()
-// 1. ... textarea tag.
-// document.querySelector("#textarea-input");
-
-// 2. ... element with a class btn-primary.
-// document.querySelector(".btn-primary");
-
-// 3. ... element with an id input-btn.
-// document.querySelector("#input-btn");
-
-// 4. ... input element with an attribute name='translator'.
-// document.querySelector("input[name='translator']");
 
 function clickHandler(){
     console.log(txtInput.value);
+    txtOutput.innerText = "hahahe " + txtInput.value;
 ;}
 
-btnTranslate.addEventListener("click", clickHandler); //clickHandler is a callback here because function addEventListener() takes an input or argument as an another function.
-
+btnTranslate.addEventListener("click", clickHandler);
