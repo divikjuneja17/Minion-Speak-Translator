421ed24a0759710da7f2e111109804887c6806e9
Divik Juneja <junejadivik17@gmail.com>
2022-09-06 22:50:30 +0530

    done live exercise - querySelector()

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ let txtInput = document.querySelector("#textarea-input");
 let txtOutput = document.querySelector("#textarea-output");
 
 
+// live exercise: querySelector()
+// 1. ... textarea tag.
+// document.querySelector("#textarea-input");
 
+// 2. ... element with a class btn-primary.
+// document.querySelector(".btn-primary");
+
+// 3. ... element with an id input-btn.
+// document.querySelector("#input-btn");
+
+// 4. ... input element with an attribute name='translator'.
+// document.querySelector("input[name='translator']");
 
 function clickHandler(){
     console.log(txtInput.value);
