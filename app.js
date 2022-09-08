53656aa420be78bff5b5cec7f17936b09a792985
Divik Juneja <junejadivik17@gmail.com>
2022-09-08 21:05:20 +0530

    live exercise: different inputText

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#input");
 let txtOutput = document.querySelector("#output");
-let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Divik Juneja"; //server URL including input to server 'I am Divik Juneja'
+let text1 = "I am Iron Man";
+let text2 = "I am Batman";
+let text3 = "I am Shaktimaan";
+
+//live exercise: different inputText using function
+function constructURL(inputText){
+    let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?" + "text=" + inputText;
+    return serverURL;
+}
 
 
 function clickHandler(){
-   //first way of using fetch() with arrow function definiton
-    // fetch(serverURL)
-    // .then(response => response.json())
-    // .then(json => console.log(json));
-
-    //second way of using fetch() with normal function definition
-    fetch(serverURL)
-    .then(function response(response){ //response parameter is passed to responseHandler() function which we have received from fetch() funcion. In responseHandler(), we are returning response in the form of json.
-         return response.json();
-    })
-    .then(function logJson(json){ //json value that we recieved from responseHandler() is being passed inside logJson() as a parameter and we are printing that json value on console.
-        console.log(json);
-    })
+    fetch(constructURL(text1))
+    .then(response => response.json())
+    .then(json => console.log(json));
 ;}
 
 btnTranslate.addEventListener("click", clickHandler);
