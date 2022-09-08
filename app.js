5d269c78defb1b657ed2375457fcea8be04c7324
Divik Juneja <junejadivik17@gmail.com>
2022-09-08 21:40:33 +0530

    added clear button functionality

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#input");
 let txtOutput = document.querySelector("#output");
+let btnClear = document.querySelector("#btn-clear");
 let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-//when we click on button, clickHandler() function executes, then getTranslatedURL() executes in which serverURL is appended with taking input of user from input text area dynamically and then URL is returned to fetch() and then fetch() returns response which is then converted to json format and then output in div get changed to 'translated' text using innerText.
+
 function getTranslatedURL(){
     return serverURL + "?" + "text=" + txtInput.value;
 }
@@ -14,4 +15,8 @@ function clickHandler(){
     .then(json => txtOutput.innerText = json.contents.translated);
 ;}
 
+function clear(){
+    txtInput.value = " ";
+}
 btnTranslate.addEventListener("click", clickHandler);
+btnClear.addEventListener("click", clear); //added clear button functionality
