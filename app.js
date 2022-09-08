49e7adc2e3194e90b1227e49a9db4fd68793313f
Divik Juneja <junejadivik17@gmail.com>
2022-09-08 20:35:43 +0530

    fetched data from server &  console log it

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#input");
 let txtOutput = document.querySelector("#output");
-
+let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Divik Juneja"; //server URL including input to server 'I am Divik Juneja'
 
 
 function clickHandler(){
-    console.log(txtInput.value);
-    txtOutput.innerText = "hahahe " + txtInput.value;
+   //first way of using fetch() with arrow function definiton
+    // fetch(serverURL)
+    // .then(response => response.json())
+    // .then(json => console.log(json));
+
+    //second way of using fetch() with normal function definition
+    fetch(serverURL)
+    .then(function response(response){ //response parameter is passed to responseHandler() function which we have received from fetch() funcion. In responseHandler(), we are returning response in the form of json.
+         return response.json();
+    })
+    .then(function logJson(json){ //json value that we recieved from responseHandler() is being passed inside logJson() as a parameter and we are printing that json value on console.
+        console.log(json);
+    })
 ;}
 
 btnTranslate.addEventListener("click", clickHandler);
